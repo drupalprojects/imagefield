d4b6bb0af98b046e5a6815120adfd40d3f710343
Nathan Haug <quicksketch@35821.no-reply.drupal.org>
2009-03-15 00:25:29 +0000

    #292542 by drewish: Match filefield.js accept property JavaScript.

--- imagefield.js
+++ imagefield.js
@@ -4,28 +4,25 @@
  *  Auto Attach standard client side file input validation
  */
 Drupal.imagefieldValidateAutoAttach = function() {
-  $("input[@type='file'].imagefield").change( function() {
+  $("input.imagefield[@type='file']").change(function() {
+    // Remove any previous errors.
     $('.imagefield-js-error').remove();
+
     /**
-     *  add client side validation for the input[@file] accept attribute
+     * Add client side validation for the input[@file] accept attribute
      */
     var accept = this.accept.replace(/,\s*/g, '|');
     if (accept.length > 1) {
       var v = new RegExp('\\.(' + accept + ')$', 'gi');
       if (!v.test(this.value)) {
-        var error = 'The file ' + this.value + " is not supported.\n";
-        error += "Only the following file types are supported: \n" + accept.replace(/\|/g, ', ');
-        alert(error);
-        // what do I prepend this to? 
-        //$(this).prepend($('<div class="imagefield-js-error>"' + error + '</div>'));
-        this.value = ''; 
-        return false;
-      }   
-    }   
-    /**
-     * Add filesize validation where possible
-     */
+        var error = 'The selected file ' + this.value + ' cannot not be uploaded. Only files with the following extensions are allowed: ' + accept.replace(/\|/g, ', ');
 
+        $(this).before('<div class="messages error file-upload-js-error">' + error + '</div>');
+        this.value = '';
+
+        return false;
+      }
+    }
   }); 
 }
 
